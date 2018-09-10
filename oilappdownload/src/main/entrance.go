b974c5522adbf2af0004886b7e172edc71c2cc69
huxn <huxn@sina.com>
2018-09-10 16:16:12 +0800

    调整包

--- oilappdownload/src/main/entrance.go
+++ oilappdownload/src/main/entrance.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"net/http"
-	"myapp"
 	"os"
 	"os/signal"
 	"log"
 	"time"
+	"fmt"
+	"net/url"
 )
 
+var filepath string
+var staticHandler http.Handler
+func init()  {
+	filepath ="/Users/huxiaoning/Downloads/app-alli-debug.apk"
 
+	staticHandler = http.FileServer(http.Dir(filepath))
+}
 
 
 
@@ -22,7 +29,7 @@ func main() {
 	signal.Notify(quit, os.Interrupt)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/downloadapp",myapp.APPdownload)
+	mux.HandleFunc("/downloadapp",APPdownload)
 
 
 	server := http.Server{
@@ -42,4 +49,30 @@ func main() {
 
 	}
 
+}
+
+
+/**
+下载文件处理
+ */
+
+func APPdownload(w http.ResponseWriter, r *http.Request){
+
+	fmt.Print(w, "开始下载...")
+	//w.Header().Set("Content-Type", "application/force-download")
+	//w.Header().Set("Content-Type","application/octet-stream")
+	w.Header().Set("Content-Disposition:", "attachment;filename="+url.QueryEscape(time.Now().Format("20060102150405")+".apk"))
+	w.Header().Set("Content-Description","File Transfer")
+	w.Header().Set("Content-Transfer-Encoding","binary")
+	w.Header().Set("Cache-Control","must-revalidate")
+
+
+
+	streamBytes, err := os.Open(filepath)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Unable to open and read file : %v", err), 500)
+		return
+	}
+	defer streamBytes.Close()
+	http.ServeContent(w, r, filepath, time.Time{}, streamBytes)
 }
